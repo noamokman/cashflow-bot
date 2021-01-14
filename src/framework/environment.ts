be6b1490f2611c44f55d0d424cdf10af882e6b4b
Noam Okman <noamokman@gmail.com>
2021-01-14 20:23:43 +0200

    feat(initial): initial boilerplate for a bot

---
+++ src/framework/environment.ts
@@ -0,0 +1,5 @@
+import env from 'env-var';
+
+export const BOT_TOKEN = env.get('BOT_TOKEN')
+  .required()
+  .asString();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('hey!');
+import {Telegraf} from 'telegraf';
+import {BOT_TOKEN} from './framework/environment';
+
+const bot = new Telegraf(BOT_TOKEN);
+
+bot.start(ctx => ctx.reply('Welcome'));
+bot.help(ctx => ctx.reply('Send me a sticker'));
+bot.on('sticker', ctx => ctx.reply('👍'));
+bot.hears('hi', ctx => ctx.reply('Hey there'));
+void bot.launch();
+
+process.once('SIGINT', () => {
+  bot.stop('SIGINT');
+});
+process.once('SIGTERM', () => {
+  bot.stop('SIGTERM');
+});
