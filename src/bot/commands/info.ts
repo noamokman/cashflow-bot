2307de3dbfae31448746448cc1717081525ab7f0
Noam Okman <noamokman@gmail.com>
2021-02-27 09:53:27 +0200

    fix(build): remove old code

--- src/bot/commands/info.ts
+++ src/bot/commands/info.ts
@@ -1,9 +1,5 @@
 import type {AppContext} from '../../types';
 
 export default async (ctx: AppContext) => {
-  const scrapers = ctx.db.get('scrapers')
-    .filter({user: ctx.from?.id})
-    .value();
-
-  await ctx.reply(JSON.stringify(scrapers));
+  await ctx.reply('info');
 };
